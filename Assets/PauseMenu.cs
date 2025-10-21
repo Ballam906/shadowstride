8bd86a59d9f52fd3f602b13468f46587754268b2
Ballam906 <90650752+Ballam906@users.noreply.github.com>
2025-10-21 21:43:22 +0200

    pausemenu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);     // Megjelenik a menü
+        Time.timeScale = 0f;             // Megállítja a játékot
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);    // Eltűnik a menü
+        Time.timeScale = 1f;             // Folytatja a játékot
+        isPaused = false;
+    }
+
+    public void Fomenu()
+    {
+        Time.timeScale = 1f; // <<< kulcsfontosságú
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f; // <<< FONTOS!
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
